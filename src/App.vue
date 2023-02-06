b74922b39b1bfe50565ee85ff4349756afea04f3
taemin <myloopmy@gmail.com>
2023-02-06 14:53:52 +0900

    간단한 모달창을 활용해서 반복문으로 임의 데이터 import후 이미지 제목 가격 뽑아내서 부동산 초간단 부동산관련 사이트 구현.

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  안녕하세요
+
+  <div class="black-bg" v-if="modalOpen == true">
+    <div class="white-bg">
+      <h4>건물소개</h4>
+      <p><b>좋은집입니다</b>.<br/> 선택하시면 후회없습니다.</p>
+      <button @click="modalOpen=false">닫기</button>
+    </div>
+  </div>
+
+
+  <div class="menu">
+    <a v-for="(item,i) in menus" :key="i">{{ item }}</a>
+  </div>
+
+  <div>
+    <h4 v-for="(item,i) in datas" :key="i">
+      <img :src="item.image" class="romm-img"><br/>
+      <div @click="modalOpen=true" class="name">{{ item.title }}</div>
+      <p>{{item.price}}원</p>
+    </h4>
+    <br/>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import oneroom from "./oneroom"
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      price: [50,70,100],
+      mainColor : 'color:purple',
+      menus: ['Home','Shop','About'],
+      products : ['역삼동원룸','천호동원룸','마포구원룸'],
+      reportNum : [0,0,0],
+      modalOpen : false,
+      datas : oneroom,
+    }
+  },
+  methods: {
+    increase() {
+      this.reportNum++;
+    },
   }
 }
 </script>
@@ -22,6 +56,42 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.romm-img{
+  width: 40%;
+  margin-top: 5%;
+  border-radius: 20px;
+  border: 1px solid purple;
+}
+
+body{
+  margin:0;
+}
+div{
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg{
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.name{
+  cursor: pointer;
 }
 </style>
