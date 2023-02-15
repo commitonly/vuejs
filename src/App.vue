cc132670a0b3feada6b59012042e35119381c5df
taemin <myloopmy@gmail.com>
2023-02-15 11:10:03 +0900

    조건문을 통해서 원하는 값만 바인딩하기, 트랜잭션 에니메이팅 효과 추가

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
 
-  <Modal :datas="datas" :pushed="pushed" :modalOpen="modalOpen"/>
-
-
+<!--  <div class="start" :class="{end : modalOpen}">-->
+<!--  <Modal @closeModal="modalOpen = false" :datas="datas" :pushed="pushed" :modalOpen="modalOpen"/>-->
+<!--  </div>-->
+  <transition name="fade" >
+  <Modal @closeModal="modalOpen = false" :datas="datas" :pushed="pushed" :modalOpen="modalOpen"/>
+  </transition>>
 
   <div class="menu">
     <a v-for="(item,i) in menus" :key="i">{{ item }}</a>
@@ -10,19 +13,16 @@
 
   <Discount/>
 
-<!--  아래는 반복문 -->
-  <Card :datas="datas[i]" v-for="(item, i) in datas" key="i" />
+  <!--  아래는 반복문 -->
+  <Card @modalOpen="modalOpen = true; pushed = $event" :datas="datas[i]" v-for="(item, i) in datas" :key="i"/>
 
-<!--  아래는 하드코딩 -->
+  <!--  아래는 하드코딩 -->
 
-<!--  <Card :datas="datas[1]" />-->
-<!--  <Card :datas="datas[2]" />-->
-<!--  <Card :datas="datas[3]" />-->
-<!--  <Card :datas="datas[4]" />-->
-<!--  <Card :datas="datas[5]" />-->
-
-
-
+  <!--  <Card :datas="datas[1]" />-->
+  <!--  <Card :datas="datas[2]" />-->
+  <!--  <Card :datas="datas[3]" />-->
+  <!--  <Card :datas="datas[4]" />-->
+  <!--  <Card :datas="datas[5]" />-->
 
 
 </template>
@@ -36,16 +36,16 @@ import Card from "@/Card.vue";
 
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
-      pushed : 0,
-      price: [50,70,100],
-      mainColor : 'color:purple',
-      menus: ['Home','Shop','About'],
-      products : ['역삼동원룸','천호동원룸','마포구원룸'],
-      reportNum : [0,0,0],
-      modalOpen : false,
-      datas : oneroom,
+      pushed: 0,
+      price: [50, 70, 100],
+      mainColor: 'color:purple',
+      menus: ['Home', 'Shop', 'About'],
+      products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      reportNum: [0, 0, 0],
+      modalOpen: false,
+      datas: oneroom,
     }
   },
   methods: {
@@ -54,9 +54,9 @@ export default {
     },
   },
   components: {
-    Discount : Discount,
-    Modal : Modal,
-    Card : Card,
+    Discount: Discount,
+    Modal: Modal,
+    Card: Card,
   }
 }
 </script>
@@ -75,26 +75,28 @@ export default {
   padding: 15px;
   border-radius: 5px;
 }
+
 .menu a {
   color: white;
   padding: 10px;
 }
 
-.romm-img{
+.romm-img {
   width: 40%;
   margin-top: 5%;
   border-radius: 20px;
   border: 1px solid purple;
 }
 
-body{
-  margin:0;
+body {
+  margin: 0;
 }
-div{
+
+div {
   box-sizing: border-box;
 }
 
-.discount{
+.discount {
   background: #eee;
   padding: 10px;
   margin: 10px;
@@ -102,18 +104,57 @@ div{
 }
 
 
-
 .black-bg {
-  width: 100%; height: 100%;
-  background: rgba(0,0,0,0.5);
-  position: fixed; padding: 20px;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
 }
-.white-bg{
-  width: 100%; background: white;
+
+.white-bg {
+  width: 100%;
+  background: white;
   border-radius: 8px;
   padding: 20px;
 }
-.name{
+
+.name {
   cursor: pointer;
+}
+
+.start{
+  opacity: 0;
+  transition: all 1s;
+}
+.end{
+  opacity: 1;
+}
+
+.fade-enter-from {
+  /*opacity: 0;*/
+  transform: translateY(-1000px);
+
+}
+
+.fade-enter-active{
+  transition: all 1s;
+}
+
+.fade-enter-to{
+  transform: translateY(0px);
+}
+
+.fade-leave-from {
+  opacity: 1;
+
+}
+
+.fade-leave-active{
+  transition: all 1s;
+}
+
+.fade-leave-to{
+  opacity: 0;
 }
 </style>
--- src/Modal.vue
+++ src/Modal.vue
@@ -1,23 +1,46 @@
 <template>
   <div class="black-bg" v-if="modalOpen == true">
     <div class="white-bg">
-      <h4>건물소개</h4>
-      <p> {{ datas[pushed].title }}</p>
       <img :src="datas[pushed].image" class="romm-img">
-      <p> {{ datas[pushed].price }}</p>
+      <h4> {{ datas[pushed].title }}</h4>
       <p> {{ datas[pushed].content }}</p>
-      <button @click="modalOpen=false">닫기</button>
+      <input v-model="month">
+<!--      <input type="range" min="1" max="12">-->
+      <p> {{ month }}개월 선택 : {{ datas[pushed].price * month }}원</p>
+      <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name : 'Modal',
-  props : {
-    datas : Array,
-    pushed : Number,
-    modalOpen : Boolean,
+  name: 'Modal',
+  data() {
+    return {
+      month: 1,
+    }
+  },
+  watch: {
+    // month(a,b){ // a 는 변경 후 데이터 b는 변경 전 데이터
+    month(a){
+      if (a >= 13) {
+        alert('13개월 이상은 불가능합니다.')
+        this.month = 1
+      }
+      if (isNaN(a)){
+        alert("숫자만 입력해주세요!")
+        this.month = 1
+      }
+      if (a == 0){
+        alert('1보다 작은 수를 입력할 수 없습니다.')
+        this.month = 1
+      }
+    },
+  },
+  props: {
+    datas: Array,
+    pushed: Number,
+    modalOpen: Boolean,
   }
 }
 </script>
