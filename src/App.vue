b5cf2d64a586e6c6237935ea86b8a0db569873dd
taemin <myloopmy@gmail.com>
2023-02-19 17:44:23 +0900

    블로그 props

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">Navbar</span>
+    </div>
+  </nav>
+  <div class="container">
+    <h5>Vue js 를 이용한 블로그입니다</h5>
+    <p>여러가지를 시도할 생각입니다</p>
+    <List :blog="blog" />
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import List from "@/components/List.vue";
+import blog from "@/assets/blog";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return{
+      blog : blog
+    }
+  },
+  components: {List}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.container {
+  margin: 0 auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
