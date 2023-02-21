f02e28dee961c7b3161f9a91b04190f9a1096cf2
taemin <myloopmy@gmail.com>
2023-02-22 00:55:40 +0900

    상세페이지로 보내기

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -3,6 +3,7 @@
     <h4>상세페이지</h4>
     <h5>{{blog[$route.params.id].title}}</h5>
     <p>{{ blog[$route.params.id].content }}</p>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import List from "./components/List.vue";
 import Home from "./components/Home.vue";
 import Detail from "./components/Detail.vue";
 import error from "@/components/Error.vue";
+import Author from "@/components/Author.vue";
+import Comment from "@/components/Comment.vue";
 
 const routes = [
 
@@ -19,6 +21,16 @@ const routes = [
         // path: "/detail/:id(\\d+)",// 정규식문법 : 숫자만 넣어라!
         // path: "/detail/:id*",// 정규식문법 : /:id/:id 이렇게 슬래시 이후로 중복해서 쳤을 때
         component: Detail,
+        children:[
+            {
+                path: "author",
+                component : Author,
+            },
+            {
+                path : "comment",
+                component: Comment,
+            },
+        ]
     },
     {
         // 이런 404페이지를 만들게 되면 최하단에 쓰는 것이 좋다. 위에 작성된 코드가 우선권을 가지니까 가장 아래에 쓰도록하자
