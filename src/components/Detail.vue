41b1c13aa8b152c762a4b4e1b836adf1b588a281
taemin <myloopmy@gmail.com>
2023-02-22 00:01:28 +0900

    정규식 404 페이지 구현 및 라우터 설정 정규식 문법사용

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-dddd
+    <h4>상세페이지</h4>
+    <h5>{{blog[$route.params.id].title}}</h5>
+    <p>{{ blog[$route.params.id].content }}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+props : {
+  blog : Array,
+}
 }
 </script>
 
--- src/router.js
+++ src/router.js
@@ -2,8 +2,10 @@ import { createWebHistory, createRouter } from "vue-router";
 import List from "./components/List.vue";
 import Home from "./components/Home.vue";
 import Detail from "./components/Detail.vue";
+import error from "@/components/Error.vue";
 
 const routes = [
+
     {
         path: "/list",
         component: List,
@@ -14,7 +16,15 @@ const routes = [
     },
     {
         path: "/detail/:id",
+        // path: "/detail/:id(\\d+)",// 정규식문법 : 숫자만 넣어라!
+        // path: "/detail/:id*",// 정규식문법 : /:id/:id 이렇게 슬래시 이후로 중복해서 쳤을 때
         component: Detail,
+    },
+    {
+        // 이런 404페이지를 만들게 되면 최하단에 쓰는 것이 좋다. 위에 작성된 코드가 우선권을 가지니까 가장 아래에 쓰도록하자
+        // 그렇지 않으면 이게 최상단이면 아래에 list detail 이런 단어가 anything 정규식에 먹힌다.
+        path: "/:anything(.*)", // 정규식 문법 : 모든 문자가 들어오면 !
+        component: error,
     },
 
 
