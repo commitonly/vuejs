0886f0075c075782313bc6dd8291b31fd19957b3
taemin <myloopmy@gmail.com>
2023-02-22 23:05:47 +0900

    초기 골격 메인페이지 디자인

---
+++ src/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Post/>
+    <Post/>
+    <Post/>
+  </div>
+</template>
+
+<script>
+import post from "@/Post.vue";
+
+export default {
+  components:{
+    Post : post,
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
