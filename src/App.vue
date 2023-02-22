b3bab8ce29439b06e8a53908bae6a683c0894c73
taemin <myloopmy@gmail.com>
2023-02-22 22:45:30 +0900

    프로젝트 시작합니다 기존 파일 다 날립니다.

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,13 @@
 <template>
-  <nav class="navbar bg-body-tertiary">
-    <div class="container-fluid">
-      <span class="navbar-brand mb-0 h1">Navbar</span>
-    </div>
-  </nav>
-
-
-  <router-link to="/list">리스트페이지</router-link><br/>
-  <router-link to="/">홈페이지</router-link><br/>
-  <router-link to="/detail">상세페이지</router-link>
-<!--  -->
-
-<div class="mt-4"> <!-- 마진 탑 4 만큼 줘라는 약어 -->
-  <router-view :blog="blog"></router-view>
-</div>
-
-
+<h1>test</h1>
+<h2>정상작동하나요?</h2>
 </template>
 
 <script>
-import blog from "@/assets/blog";
-import home from "@/components/Home.vue";
 
 export default {
-  name: 'App',
-  data() {
-    return{
-      blog : blog,
-      home : home,
-    }
-  },
-  components: {
-  }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  text-align: center;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,4 @@
 import { createApp } from 'vue'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
-import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App).mount('#app')
