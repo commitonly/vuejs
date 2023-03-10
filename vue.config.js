5f8c9694962fb14834e982966fa82e23bf5de79e
taemin <myloopmy@gmail.com>
2023-03-10 23:34:03 +0900

    Progressive Web App & 셋팅

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,14 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false
 })
+
+module.exports = {
+  pwa: {
+    name: '님 앱이름',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    workboxOptions: {
+      exclude: [/\.map$/, /manifest\.json$/, 'index.html']
+    }
+  }
+}
