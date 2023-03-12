3efc77373e8786a84a04d2bd8b714b70d4b044e4
taemin <myloopmy@gmail.com>
2023-03-12 21:42:38 +0900

    Composition API2

--- src/MyPage.vue
+++ src/MyPage.vue
@@ -2,22 +2,48 @@
   <div style="padding : 10px">
     <h4>팔로워</h4>
     <input placeholder="?"/>
-    <div class="post-header">
-      <div class="profile" style="background-image: url()"></div>
-      <span class="profile-name">{{ follower }}</span>
+    <div class="post-header" v-for="(a,i) in follower"  :key="i">
+      <div class="profile" :style="`background-image: url(${a.url})`"></div>
+      <span class="profile-name">{{ a.name }}</span>
     </div>
   </div>
 </template>
 
 <script>
-import {onMounted, ref} from "vue";
+import {computed, onMounted, reactive, ref, toRefs, watch} from "vue";
 import axios from "axios";
+import {useStore} from "vuex";
 
 export default {
   name: 'mypage',
+  props: {
+    one : Number,
+  },
+
   setup() {
     // reference를 만들자
     let follower = ref([]);
+    // // let test = reactive({ name : 'kim' })
+    //
+    // let {one} = toRefs(props);
+    // one.value
+    // console.log(one.value);
+    //
+    // let result = computed( ()=> {
+    //   return follower.value.length
+    // })
+    //
+    // console.log(result.value)
+    //
+    // let store = useStore();
+    // console.log(store.state.name)
+    // console.log(store.commit())
+    //
+    // watch(one, ()=>{
+    //
+    // })
+
+
 
     onMounted(() => {
       axios.get('/follower.json').then((a) => {
