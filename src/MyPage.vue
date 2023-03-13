bea1abff83c5560e085f7668e733806bc9244e84
taemin <myloopmy@gmail.com>
2023-03-13 23:20:38 +0900

    코트카타 아닌 현재 뷰에 SPRING BOOT 얹어보자

--- src/MyPage.vue
+++ src/MyPage.vue
@@ -45,6 +45,14 @@ export default {
 
 
 
+    // 기능개발을 고도화해서 기본적인 CRUD에서 벗어나 서버와 연동을 해야함.
+    // 현재 사내 코드 기능구현이 시급해서 잠정중단.
+    //  데이터바인딩, methods, watch, component, props, event, mitt, v-if, v-for 기능 숙지 숙달
+    // 스프링부트와 VUE를 활용해서 제대로 된 사이트 하나 만드는 것이 목표
+    // 현재 스터디 및 업무 과중으로 인해 일시중단되었지만 이번에는 코드카타가 아니라 제대로 해보고 싶다.
+
+
+
     onMounted(() => {
       axios.get('/follower.json').then((a) => {
         follower.value = a.data;
